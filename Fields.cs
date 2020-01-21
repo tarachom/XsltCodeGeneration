33ab091d1e8d89f2b86c83a15919ad1808c4285f
Tarchom Tarik <tarachom@i.ua>
2020-01-21 16:00:41 +0200

    up

--- Fields.cs
+++ Fields.cs
@@ -3,7 +3,8 @@
  * Автоматично згенерований код
  *
  */
- 
+
+//Клас Test
 public class Test
 {
     public Test()
@@ -11,12 +12,16 @@ public class Test
     
     }
       
+    //Назва поля
     public string Name { get; set; }
       
+    //Опис поля
     public string Desc { get; set; }
       
+    //Кількість
     public int Count { get; set; }
       
+    //Додаткова інформація
     public string Info { get; set; }
       
 }
--- Fields.xslt
+++ Fields.xslt
@@ -16,6 +16,7 @@ public class Test
     
     }
       <xsl:for-each select="root/fields/field">
+    //<xsl:value-of select="comment"/>
     public <xsl:value-of select="type"/><xsl:text> </xsl:text><xsl:value-of select="name"/> { get; set; }
       </xsl:for-each>
 }
